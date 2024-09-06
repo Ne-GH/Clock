7baba9694cfcdb5ff3d3016790a203fb1caad715
ne-gh <printfne@gmail.com>
2024-09-06 10:26:32 +0800

    从项目中删除构建中间项

--- cmake-build-debug/.qt/QtDeployTargets.cmake
+++
@@ -1,4 +0,0 @@
-set(__QT_DEPLOY_TARGET_Module_FILE /home/yongheng/Clock/cmake-build-debug/libModule.a)
-set(__QT_DEPLOY_TARGET_Module_TYPE STATIC_LIBRARY)
-set(__QT_DEPLOY_TARGET_Clock_FILE /home/yongheng/Clock/cmake-build-debug/Clock)
-set(__QT_DEPLOY_TARGET_Clock_TYPE EXECUTABLE)
